e1212684f0c1f7ccf6d23dbbc50c5e176fa3cc82
priyanshu verma <priyanshubalram19112001@gmail.com>
2022-08-22 23:36:57 +0530

    data structure

---
+++ Searching/iterative_binary_search.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int bsearch(int arr[], int n, int x)
+{
+    int low = 0, high = n - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (arr[mid] == x)
+            return mid;
+        
+        else if (arr[mid] > x)
+            high = mid - 1;
+
+        else
+            low = mid + 1;
+        
+    }
+    return -1;
+    
+}
+
+int main()
+{
+    int arr[] = {10, 20, 30, 40, 50, 60};
+    int n = sizeof(arr);
+    int x = 20;
+    // int x = 25;
+    cout<<bsearch( arr,  n, x);
+    
+
+    return 0;
+}
