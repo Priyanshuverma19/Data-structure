24dffe2c1a909fc34a062977c88d6286a9b0c44f
Priyanshuverma19 <95571076+Priyanshuverma19@users.noreply.github.com>
2022-11-10 18:12:11 +0530

    first commit

---
+++ Link list/reverse_ll.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+using namespace std;
+class node{
+    public: 
+    int data;
+    node*next;
+    node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+void insert(node*&head, int val){
+    node*n = new node(val);
+    if (head ==NULL){
+        head = n;
+        return;
+    }
+    node*temp = head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+void display(node*head){
+    node*temp = head;
+    while(temp != NULL){
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    }
+}
+// ITERATIVE
+node* reverseit(node*&head){
+    node*prevptr = NULL;
+    node*currptr = head;
+    node*nextptr;
+    while(currptr!= NULL){
+        nextptr = currptr->next;
+        currptr->next = prevptr;
+        prevptr = currptr;
+        currptr = nextptr;
+
+    }
+    return prevptr;
+}
+
+
+//RUCURCIVE
+// node*reverse_recursiv(node*&head){
+//     if(head != NULL|| head->next != NULL){
+//        return head;
+//     }
+//      node*newhead = reverse_recursiv(head->next);
+//      head->next->next =head;
+//      head->next = NULL;
+//      return newhead;
+// }
+int main(){
+    node*head = NULL;
+    insert(head,1);
+    insert(head,2);
+    insert(head,3);
+    insert(head,4);
+    // display(head);
+    node*newhead =    reverseit(head);
+    display(newhead);
+    // node*newhead = reverse_recursiv(head);
+    // display(newhead);
+    return 0 ;
+}
