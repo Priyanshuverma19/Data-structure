5fd248ba34c0bb9b9f88cdc4550425bc38a95fd1
Priyanshuverma19 <95571076+Priyanshuverma19@users.noreply.github.com>
2022-11-13 01:32:25 +0530

    first commit

---
+++ Stack/balance_parenthesis.cpp
@@ -0,0 +1,65 @@
+
+#include<iostream>
+
+#include<stack>
+
+using namespace std;
+bool is_valid(string s){
+    int n = s.size();
+    stack<char> st;
+    bool ans = true;
+    for(int i=0; i<n; i++){
+        if(s[i]=='{'|| s[i] =='('|| s[i]=='['){
+            st.push(s[i]);
+        }
+        else if(s[i]==')'){
+            if(!st.empty()&& st.top()=='('){
+                st.pop();
+            }
+            else{
+                ans = false;
+                break;
+
+            }
+            
+        }
+        else if(s[i]==']'){
+            if(!st.empty()&& st.top()=='['){
+                st.pop();
+            }
+            else{
+                ans = false;
+                break;
+
+            }
+            
+        }
+        else if(s[i]=='}'){
+            if(!st.empty()&& st.top()=='{'){
+                st.pop();
+            }
+            else{
+                ans = false;
+                break;
+
+            }
+            
+        }
+    }
+    if(!st.empty()){
+        return false;
+    }
+
+}
+    
+int main(){
+     string s = "{[()]}";
+     if(is_valid(s)){
+        cout<<"valid string";
+
+     }
+     else{
+        cout<<"invalid string";
+}
+    return 0;
+}
